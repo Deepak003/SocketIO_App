0b9202e232c7587ec9a08337e8bfecb8e8959e3a
DEEPAK TIWARI <deepak.tiwari@ust-global.com>
2019-04-24 11:44:18 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,45 +3,64 @@ var express = require('express');
 var app = express();  
 var server = require('http').createServer(app); 
 var io = require('socket.io')(server); 
-
 //keep track of how times clients have clicked the button
 var clickCount = 0;
+var msgfromServer1="";
 var msgfromServer2="";
 app.use(express.static(__dirname + '/public')); 
 //redirect / to our index.html file
 app.get('/', function(req, res,next) {  
     res.sendFile(__dirname + '/public/index.html');
 });
-
-/* io.on('connection', function(client) {  
-	//when the server receives clicked message, do this
-    client.on('clicked', function(data) {
-    	  clickCount++;
-		  //send a message to ALL connected clients
-		  io.emit('buttonUpdate', clickCount);
-    });
-}); */
-
-
 //var io = require('socket.io').listen(3000);
 io.on('connection', function (socket) {
-    console.log('connected:', socket.client.id);
-    socket.on('serverEvent', function (data) {
-        console.log('new message from client:', data);
-		msgfromServer2 = 'new message from client:'+ data;
+    console.log('connected to 3000:', socket.client.id);
+    socket.on('clientEvent-3001', function (data) {   
+		msgfromServer2 = '[clientEvent-3001] [Inbox] :'+ data;
+		console.log(msgfromServer2);
     });
-	socket.on('clicked', function(data) {
+	socket.on('addConnection', function(data) {
     	  clickCount++;
 		  //send a message to ALL connected clients
 		  io.emit('buttonUpdate', clickCount+"##S E R V E R - 1/3000##"+msgfromServer2);
     });
     setInterval(function () {
-        socket.emit('clientEvent', Math.random());
-        console.log('message sent to the clients');
-    }, 60000);
+		var rndNum =Math.random();
+        socket.emit('serverEvent-3000', rndNum);
+        console.log('[serverEvent-3000] [Sent ] :'+rndNum);
+    }, 6000);
 });
 
 //start our web server and socket.io server listening
 server.listen(3000, function(){
   console.log('listening on *:3000');
 }); 
+
+var io1 = require('socket.io-client');
+var socket = io1.connect("http://localhost:3001/", {
+    reconnection: true
+});
+
+socket.on('connect', function () {
+    //console.log('connected to localhost:3000');
+    socket.on('serverEvent-3001', function (data) { 
+	    console.log("[serverEvent-3001] [Inbox] :"+data);
+		msgfromServer1 = "thanks server(3001)! for '" + data + "'";
+		socket.emit('clientEvent-3000', msgfromServer1);
+		console.log('[clientEvent-3000] [Sent ] :'+msgfromServer1);
+    });
+});
+
+var io2 = require('socket.io-client');
+var socket2 = io2.connect("http://localhost:3002/", {
+    reconnection: true
+});
+
+socket2.on('connect', function () {
+    socket2.on('serverEvent-3002', function (data) { 
+        console.log("[serverEvent-3002] [Inbox] :"+data);	
+        msgfromServer2 = "thanks server(3002)! for '" + data + "'";	
+        socket2.emit('clientEvent-3001/3002',msgfromServer2);
+		console.log('[clientEvent-3001/3002] [Sent ] :'+msgfromServer2);
+    });
+});
--- server1.js
+++ server1.js
@@ -7,6 +7,7 @@ var io = require('socket.io')(server);
 //keep track of how times clients have clicked the button
 var clickCount = 0;
 var msgfromServer1="";
+var msgfromServer1="";
 app.use(express.static(__dirname + '/public')); 
 //redirect / to our index.html file
 app.get('/', function(req, res,next) {  
@@ -15,11 +16,21 @@ app.get('/', function(req, res,next) {
 
 io.on('connection', function(client) {  
 	//when the server receives clicked message, do this
-    client.on('clicked', function(data) {
+	console.log('connected to 3001:', client.client.id);
+	client.on('clientEvent-3000', function (data) {   
+		msgfromServer2 ='[clientEvent-3000] [Inbox] :'+ data;
+		console.log(msgfromServer2);
+    });
+    client.on('addConnection', function(data) {
     	  clickCount++;
 		  //send a message to ALL connected clients
 		  io.emit('buttonUpdate', clickCount+"##S E R V E R - 2/3001##"+msgfromServer1);
     });
+	setInterval(function () {
+		var rndNum =Math.random();
+        client.emit('serverEvent-3001', rndNum);
+        console.log('[serverEvent-3001] [Sent ] :'+rndNum);
+    }, 6000);
 });
 
 //start our web server and socket.io server listening
@@ -33,10 +44,26 @@ var socket = io1.connect("http://localhost:3000/", {
 });
 
 socket.on('connect', function () {
-    console.log('connected to localhost:3000');
-    socket.on('clientEvent', function (data) {
-        console.log('message from the server:', data);
-        socket.emit('serverEvent', "thanks server! for sending '" + data + "'");
-		msgfromServer1 = "thanks server! for sending '" + data + "'";
+    //console.log('connected to localhost:3000');
+    socket.on('serverEvent-3000', function (data) { 
+        console.log("[serverEvent-3000] [Inbox] :"+data);	
+        msgfromServer1 = "thanks server(3000)! for '" + data + "'";	
+        socket.emit('clientEvent-3001',msgfromServer1);
+		console.log('[clientEvent-3001] [Sent ] :'+msgfromServer1);
+    });
+});
+
+var io2 = require('socket.io-client');
+var socket2 = io2.connect("http://localhost:3002/", {
+    reconnection: true
+});
+
+socket2.on('connect', function () {
+    //console.log('connected to localhost:3000');
+    socket2.on('serverEvent-3002', function (data) { 
+        console.log("[serverEvent-3002] [Inbox] :"+data);	
+        msgfromServer2 = "thanks server(3002)! for '" + data + "'";	
+        socket2.emit('clientEvent-3001/3002',msgfromServer2);
+		console.log('[clientEvent-3001/3002] [Sent ] :'+msgfromServer2);
     });
 });
